fb582d220cbdb25cd6ecd1cbb67fb4fc330bbc68
gabriel090 <gabriel.mbatha@gmail.com>
2022-02-08 13:21:34 +0300

    HomeFragment page  created

--- app/src/main/java/com/gabu/taskmanagement/ui/MainActivity.java
+++ app/src/main/java/com/gabu/taskmanagement/ui/MainActivity.java
@@ -5,7 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.gabu.taskmanagement.R;
+import com.gabu.taskmanagement.fragments.HomeFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import hotchemi.android.rate.AppRate;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,5 +18,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         BottomNavigationView bottomNav = findViewById(R.id.bottomNav);
        // bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        // show rate app prompt
+        AppRate.with (this)
+                .setInstallDays(1)
+                .setLaunchTimes(3)
+                .setRemindInterval(2)
+                .monitor();
+        AppRate.showRateDialogIfMeetsConditions(this);
+        // this checks to see if there's any savedInstance, if null, then it replaces the fragment container
+        // with home fragment.
+        if (savedInstanceState == null){
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragmentcontainer, new HomeFragment()).commit();
+        }
+
+
+
     }
 }
